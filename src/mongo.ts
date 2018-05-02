237310d67ba00a7e8f907661014572d5493b37c0
zhufeng <zhfjyq@163.com>
2018-05-02 08:58:09 +0800

    OK

--- src/mongo.ts
+++ src/mongo.ts
@@ -171,6 +171,7 @@ export class Mongo<T extends IDbSchemas> {
     // 创建其他数据库中的colls，不使用K作为外部数据库名称，使用_extDb的col名称
     for (const k of Object.keys(externCollDefines)) {
       const v = externCollDefines[k];
+      if (!v) continue;
 
       const extDbInfo: IExtDb = _.get(v, 'collOptions._extDb');
       if ((!this._client) || (!extDbInfo)) return;
